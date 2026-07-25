9bd5062c74ad0b2a4b74436900bc5a7cb8360920
Pablo Ferraz <pablo.f.ferraz@gmail.com>
2026-07-25 19:53:15 -0300

    Switch project entry to Expo Router

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function MenuScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Color Test</Text>
+    </View>
+  );
+}
